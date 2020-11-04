bee3dddb1a59e7e5f3ff36312c16b847a2f90290
Ahmet OZER <git@ahmetozer.org>
2020-11-04 20:39:25 +0300

    first version

--- asssets/js/site.js
+++ asssets/js/site.js
@@ -8,10 +8,20 @@ String.prototype.inikv = function (regxy) {
 }
 
 const ipVersions = [ "IPv4", "IPv6" ]
+function copyFunction(elementName) {
+    var copyText = document.getElementById(elementName);
+    var textArea = document.createElement("textarea");
+    textArea.value = copyText.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    textArea.setSelectionRange(0, 99999)
+    document.execCommand("Copy");
+    textArea.remove();
+}
 
-ipVersions.forEach(element => {
+ipVersions.forEach(ipVersion => {
     $.ajax({
-        url: 'https://'+siteConfig.testHosts[element]+'/cdn-cgi/tracert',
+        url: 'https://'+siteConfig.testHosts[ipVersion]+'/cdn-cgi/tracert',
         dataType: 'text',
         timeout: 1500,
         start_time: new Date().getTime(),
@@ -19,21 +29,24 @@ ipVersions.forEach(element => {
             let tempDate = new Date();
             let clientResponseDate = data.inikv("ts").split('.').join("")
             console.log(data.inikv("ip"))
-            $(".client-" + element + "-addr").html(data.inikv("ip"))
-            $(".client-" + element + "-loc").html(data.inikv("loc"))
+            $(".client-" + ipVersion + "-addr").html(data.inikv("ip"))
+            $(".client-" + ipVersion + "-addr").val(data.inikv("ip"))
+            $(".client-" + ipVersion + "-loc").html(data.inikv("loc"))
             
-            performance.getEntries().forEach(element2 => {
+            performance.getEntries().forEach(pElement => {
                 
-                if (element2.name == 'https://' + siteConfig.testHosts[element] + '/cdn-cgi/tracert') {
+                if (pElement.name == 'https://' + siteConfig.testHosts[ipVersion] + '/cdn-cgi/tracert') {
                     let ajaxDuration = tempDate - this.start_time
-                    let browserDuration = element2.duration
+                    let browserDuration = pElement.duration
                     let diff = ajaxDuration - browserDuration
-                    $(".client-" + element + "-latency").html(Math.round(browserDuration - diff)  + ' ms');
+                    $(".client-" + ipVersion + "-latency").html(Math.round(browserDuration - diff) + ' ms');
+                    $( ".if-"+ipVersion+"-ok" ).prop( "disabled", false );
                 }
 
             });
         },
         error: function (data) {
+            $( ".if-"+ipVersion+"-ok" ).prop( "disabled", true );
             console.log(data)
         }
       });
